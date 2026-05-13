f8361e7cfe87deaf3a772d21503c34e3b1416f74
Widmo206 <widmo206@yahoo.com>
2026-05-13 16:54:38 +0200

    Add __repr__ for reference types

--- pyscript_types.py
+++ pyscript_types.py
@@ -10,6 +10,17 @@ from dataclasses import dataclass
 from typing import Type, Any, Callable, TypeAlias
 
 
+def format_type(type: Type) -> str:
+    """Turn a Type into a string.
+    
+    Fix for Any showing up as typing.Any in the logs
+    """
+    try:
+        return type.__name__
+    except AttributeError:
+        return str(type)
+
+
 @dataclass(frozen=True)
 class Constant(object):
     name: str
@@ -19,6 +30,12 @@ class Constant(object):
     def __post_init__(self):
         assert isinstance(self._value, self.type)
 
+    def __repr__(self):
+        if self._value is None:
+            return f"const {self.name}: {format_type(self.type)}"
+        else:
+            return f"const {self.name}: {format_type(self.type)} = {self._value}"
+
     def get(self) -> Any:
         return self._value
 
@@ -27,10 +44,16 @@ class Constant(object):
 class Variable(object):
     name: str
     type: Type
-    _value: Any | None
+    _value: Any
 
     def __post_init__(self):
         assert self._value is None or isinstance(self._value, self.type)
+
+    def __repr__(self):
+        if self._value is None:
+            return f"var {self.name}: {format_type(self.type)}"
+        else:
+            return f"var {self.name}: {format_type(self.type)} = {self._value}"
 
     def get(self) -> Any:
         return self._value
@@ -45,6 +68,9 @@ class ExternalFunction(object):
     name: str
     return_type: Type
     function: Callable
+    
+    def __repr__(self):
+        return f"func {self.name}() -> {format_type(self.return_type)}"
 
     def call(self, *args) -> Any:
         result = self.function(*args)
@@ -58,6 +84,9 @@ class Function(object):
     name: str
     return_type: Type
     function: Callable
+
+    def __repr__(self):
+        return f"func {self.name}() -> {format_type(self.return_type)}"
 
     def call(self, *args) -> Any:
         result = self.function(*args)
