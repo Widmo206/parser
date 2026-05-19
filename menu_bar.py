3d6f51e2a26c78c168579811458a2b3be79cb0da
Romcodewasunavailable <120272499+Romcodewasunavailable@users.noreply.github.com>
2026-05-19 14:48:18 +0200

    editor tab state rework, implemented text undo/redo

--- menu_bar.py
+++ menu_bar.py
@@ -10,12 +10,13 @@ import tkinter as tk
 import ttkbootstrap as ttk
 import ttkbootstrap.constants as ttkc
 
-from menu_command import FileMenuCommand, ViewMenuCommand
+from menu_command import EditMenuCommand, FileMenuCommand, ViewMenuCommand
 
 
 class MenuBar(ttk.Frame):
-    file_menu: ttk.Menu
-    view_menu: ttk.Menu
+    file_menu_button: ttk.Menubutton
+    edit_menu_button: ttk.Menubutton
+    view_menu_button: ttk.Menubutton
 
     def __init__(self, master: tk.Misc, **kwargs) -> None:
         kwargs.setdefault("bootstyle", ttkc.DARK)
@@ -24,6 +25,10 @@ class MenuBar(ttk.Frame):
         self.file_menu_button = ttk.Menubutton(self, text="File", bootstyle=kwargs["bootstyle"])
         self.file_menu_button.grid(column=0, row=0)
         FileMenuCommand.set_menu(self.file_menu_button)
+
+        self.edit_menu_button = ttk.Menubutton(self, text="Edit", bootstyle=kwargs["bootstyle"])
+        self.edit_menu_button.grid(column=1, row=0)
+        EditMenuCommand.set_menu(self.edit_menu_button)
 
         self.view_menu_button = ttk.Menubutton(self, text="View", bootstyle=kwargs["bootstyle"])
         self.view_menu_button.grid(column=2, row=0)
